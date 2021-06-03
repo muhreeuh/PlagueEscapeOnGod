4d6e0f4a31f8d54a5c1c362eff582f5ee5ed073a
FranciscoCosta47 <Franciscocosta0306@gmail.com>
2021-06-03 16:56:49 +0100

    update Countdown Timmer

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 000f;
+    float startingTime = 180f;
+
+    [SerializeField] Text countdownText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString ("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+}
