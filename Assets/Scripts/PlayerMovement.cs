98a4b24bef94822590578a44a6eb3c632aecd059
FranciscoCosta47 <Franciscocosta0306@gmail.com>
2021-06-03 22:19:47 +0100

    update player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+   [SerializeField] float velocidade = 4f;
+    Vector3 frontal, lateral;
+
+    void Start()
+    {
+        frontal = transform.forward;
+        lateral = Quaternion.Euler(new Vector3(0, 90, 0)) * frontal;
+    }
+
+    
+    void Update()
+    {
+        Vector3 horizontal = lateral * velocidade * Time.deltaTime * Input.GetAxis("Horizontal");
+        Vector3 vertical = frontal * velocidade * Time.deltaTime * Input.GetAxis("Vertical");
+
+        Vector3 movimento = horizontal + vertical;
+        transform.position += movimento;
+        transform.LookAt(transform.position + movimento);
+
+
+    }
+}
