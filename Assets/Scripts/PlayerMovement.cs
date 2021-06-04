c33c5b44c9ba5ce171830ba48dec8191f0065b27
PatAm16 <patricia.amaral16@gmail.com>
2021-06-04 09:31:41 +0100

    Update respawn.patricia

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,8 +8,14 @@ public class PlayerMovement : MonoBehaviour
    [SerializeField] float velocidade = 4f;
     Vector3 frontal, lateral;
 
+    Vector3 playerOriginalPosition;
+    Quaternion playerOriginalOrientation; 
+
     void Start()
     {
+        playerOriginalPosition = transform.position;
+        playerOriginalOrientation = transform.rotation;
+
         frontal = transform.forward;
         lateral = Quaternion.Euler(new Vector3(0, 90, 0)) * frontal;
     }
@@ -24,6 +30,14 @@ public class PlayerMovement : MonoBehaviour
         transform.position += movimento;
         transform.LookAt(transform.position + movimento);
 
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Respawn"))
+        {
+            transform.position = playerOriginalPosition;
+            transform.rotation = playerOriginalOrientation;
+        }
     }
 }
