584f6fc8c641e096fba7fcfcee3386ae734eeb0d
FranciscoCosta47 <Franciscocosta0306@gmail.com>
2021-06-05 19:31:25 +0100

    Update AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    NavMeshAgent agente;
+    Transform alvo;
+   
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+
+        alvo = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    
+    void Update()
+    {
+        agente.destination = alvo.position;
+    }
+}
