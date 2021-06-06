914dfe9231f48c84b23daa55e07e7b3abb179b3a
FranciscoCosta47 <Franciscocosta0306@gmail.com>
2021-06-06 13:42:12 +0100

    Update Vida

---
+++ Assets/Scripts/Vida.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vida : MonoBehaviour
+{
+    public static bool JogoAcabou;
+
+    [SerializeField]
+    GameObject MenuGameOver;
+
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 15) * Time.deltaTime * 3);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.name =="Player")
+        {
+            
+            Destroy(gameObject);
+        }
+
+    }
+}
