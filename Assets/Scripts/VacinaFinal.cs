0a819be36c0127a88b376f7dc065111b4f3fbcb6
FranciscoCosta47 <Franciscocosta0306@gmail.com>
2021-06-06 12:08:11 +0100

    Update Vacinas Menus

---
+++ Assets/Scripts/VacinaFinal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VacinaFinal : MonoBehaviour
+{
+    
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.name == "Player")
+        {
+
+            CreditosFinais();
+        }
+
+
+    }
+
+    public void CreditosFinais()
+    {
+
+        SceneManager.LoadScene("Menu_Creditos");
+
+
+    }
+}
