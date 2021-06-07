a1809ad3428c2553fb67fc59ce91d6444c6901f1
muhreeuh <muhreeuh_maria@users.noreply.github.com>
2021-06-07 01:37:22 +0100

    .

---
+++ Assets/Scripts/VidaScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VidaScript : MonoBehaviour
+{
+  
+    public static bool JogoAcabou;
+
+    [SerializeField]
+    GameObject MenuGameOver;
+
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.name == "Player")
+        {
+
+            Destroy(gameObject);
+        }
+
+    }
+}
