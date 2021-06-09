c9da8f4c6b762f6438fa392374ee587fc634ab1e
muhreeuh <muhreeuh_maria@users.noreply.github.com>
2021-06-09 21:38:28 +0100

    .

---
+++ Assets/Scripts/Bacteria.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bacteria : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        void Update()
+        {
+            transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
