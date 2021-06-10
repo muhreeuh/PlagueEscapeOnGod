faa33c41835c0503ccd2a88d15eeb400d543136d
FranciscoCosta47 <Franciscocosta0306@gmail.com>
2021-06-10 12:36:01 +0100

    Update Temporizador

---
+++ Assets/Scripts/Temporizador.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temporizador : MonoBehaviour
+
+{
+    public float timeRemaining = 5;
+
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+
+        else
+        {
+
+        }
+
+    }
+    
+
+
+}
+
